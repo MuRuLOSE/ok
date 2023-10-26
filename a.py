853241b1690f45a709c47f743b41c0e7195bae70
Macsim <134152147+MuRuLOSE@users.noreply.github.com>
2023-10-26 17:27:57 +0300

    Update a.py

--- a.py
+++ a.py
@@ -4,18 +4,16 @@ class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
             file_path = self.path[1:]
-            if not file_path.startswith('modules/'):
-                raise FileNotFoundError
             file_content = open(file_path, encoding='utf-8').read()
             self.send_response(200)
             self.send_header('Content-type', 'text/plain; charset=utf-8')
             self.end_headers()
             self.wfile.write(bytes(file_content, 'utf-8'))
         except FileNotFoundError:
-            self.send_response(404)
+            self.send_response(403)
             self.send_header('Content-type', 'text/plain; charset=utf-8')
             self.end_headers()
-            self.wfile.write(bytes("Файл не найден", 'utf-8'))
+            self.wfile.write(bytes("Вы вышли за пределы, чтобы найти модуль надо сделать так: url/modules/author/test.py", 'utf-8'))
 
 def run():
     print('Запуск сервера...')
