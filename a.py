0adea8f29b9e3860377c3e394638d9b8a6398732
Macsim <134152147+MuRuLOSE@users.noreply.github.com>
2023-10-26 16:35:22 +0300

    Add files via upload

---
+++ a.py
@@ -0,0 +1,30 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            file_path = self.path[1:]
+            if file_path.startswith('modules') or file_path.startswith('/modules'):
+                pass
+            else:
+                return self.send_response(403,"Куда полезли?)")
+            file_content = open(file_path, encoding='utf-8').read()
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(bytes(file_content, 'utf-8'))
+        except FileNotFoundError:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(bytes("Файл не найден", 'utf-8'))
+
+def run():
+    print('Запуск сервера...')
+    server_address = ('', 8080)
+    httpd = HTTPServer(server_address, MyHandler)
+    print('Сервер работает на порту 8080')
+
+    httpd.serve_forever()
+
+run()
